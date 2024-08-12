8bb68d3b390e6994f1db4a95c4e87b68878e7184
Jan Macku <jamacku@redhat.com>
2024-08-12 12:40:25 +0200

    fix: allow to store API key in environment variable
    `cwd/.env` and `cwd/.env.storypointer` are supported.

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,9 @@
 export function raise(error: string): never {
   throw new Error(error);
 }
+
+export function tokenUnavailable(): never {
+  return raise(
+    `JIRA_API_TOKEN not set.\nPlease set the JIRA_API_TOKEN environment variable in ~/.env.storypointer or ~/.env.`
+  );
+}
