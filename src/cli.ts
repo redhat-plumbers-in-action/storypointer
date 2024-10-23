ffa1f62f0beb3d1d4b5b36460c02ad96ca5d8c78
Jan Macku <jamacku@redhat.com>
2024-10-23 09:30:07 +0200

    1.7.0

--- src/cli.ts
+++ src/cli.ts
@@ -31,7 +31,7 @@ export function cli(): Command {
     .description(
       '📐 Small CLI tool to set JIRA Story Points, Priority and Severity'
     )
-    .version('1.6.1');
+    .version('1.7.0');
 
   program
     .option(
